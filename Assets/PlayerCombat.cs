034a8ade35970ad3d9049d358d5090bdb41d51b1
Omer Ahmed Khan <oxk4769@mavs.uta.edu>
2023-11-29 20:06:18 -0600

    added villain damage

---
+++ Assets/PlayerCombat.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+	public Animator animator; 
+	public Transform attackPoint;
+	public float attackRange = .5f; 
+	public LayerMask enemyLayers;
+	public int attackDamage = 40;
+
+   void Update()
+   {
+
+   		if(Input.GetKeyDown(KeyCode.Space))
+   		{
+   			Attack();
+   		}
+   }
+
+   void Attack()
+   {
+   		animator.SetTrigger("Attack"); 
+
+   		Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+   		foreach(Collider2D enemy in hitEnemies)
+   		{
+   			enemy.GetComponent<Enemy>().TakeDamage(attackDamage); 
+   		}
+   }
+
+   void OnDrawGizmosSelected()
+   {
+   		if(attackPoint == null)
+   			return; 
+
+   	Gizmos.DrawWireSphere(attackPoint.position, attackRange); 
+   }
+}
